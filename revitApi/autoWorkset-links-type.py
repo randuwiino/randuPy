a8831c5ed0d5e7eb5a87a383a53009c99347542b
randuwiino <56397355+randuwiino@users.noreply.github.com>
2019-10-16 18:36:59 +0800

    System.MissingMemberException: Name

--- revitApi/autoWorkset-links-type.py
+++ revitApi/autoWorkset-links-type.py
@@ -17,7 +17,7 @@ def GetWorkset(name):
 
 def GetElem(singleCategory):
 	col = FilteredElementCollector(doc)
-	return col.OfCategory(singleCategory).WhereElementIsNotElementType().ToElements()
+	return col.OfCategory(singleCategory).WhereElementIsElementType().ToElements()
 
 t = Transaction(doc, "Transaction Name")
 t.Start()
@@ -29,36 +29,9 @@ links = GetElem(BuiltInCategory.OST_RvtLinks)
 for l in links:
 	name = l
 	param = l.get_Parameter(BuiltInParameter.ELEM_PARTITION_PARAM)
-	if "-ARC-" in l.Name:
+	if param.IsReadOnly: continue
+	if "-BRDG-" in l.Name:
 		param.Set(GetWorkset("Links-ARC"))
-	elif "-STR-" in l.Name:
-		param.Set(GetWorkset("Links-STR"))
-	elif "-BDR-" in l.Name:
-		param.Set(GetWorkset("Links-MEP (BDR)"))
-	elif "-BEL-" in l.Name:
-		param.Set(GetWorkset("Links-MEP (BEL)"))
-	elif "-BFS-" in l.Name:
-		param.Set(GetWorkset("Links-MEP (BFS)"))
-	elif "-BME-" in l.Name:
-		param.Set(GetWorkset("Links-MEP (BME)"))
-	elif "-BPL-" in l.Name:
-		param.Set(GetWorkset("Links-MEP (BPL)"))
-	elif "-MDE-" in l.Name:
-		param.Set(GetWorkset("Links-MEP (MDE)"))
-	elif "-BTG-" in l.Name:
-		param.Set(GetWorkset("Links-MEP (BTG)"))
-	elif "-ELV-" in l.Name:
-		param.Set(GetWorkset("Links-MEP (ELV)"))
-	elif "-ICT-" in l.Name:
-		param.Set(GetWorkset("Links-MEP (ICT)"))
-	elif "-LTE-" in l.Name:
-		param.Set(GetWorkset("Links-MEP (LTE)"))
-	elif "-CEL-" in l.Name:
-		param.Set(GetWorkset("Links-Ceiling"))
-	elif "-STE-" in l.Name:
-		param.Set(GetWorkset("Links-Site"))
-	elif "-BAL-" in l.Name:
-		param.Set(GetWorkset("Links-Clash Balls"))
     
 t.Commit()
 print ("Well done!")
